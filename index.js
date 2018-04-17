70108ecc2cbae3702c7840ad28f8f41bd322a54b
jaredianmills <jaredianmills@gmail.com>
2018-04-17 19:17:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,6 +50,6 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    
+    var orderPlaced = `Your total cost is $${total()}, which will be charged to the card 83296759.`
   }
 }
