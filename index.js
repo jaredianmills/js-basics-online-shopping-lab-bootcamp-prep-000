a0073a3b4026116a30531c2f385f14191aa77fc0
jaredianmills <jaredianmills@gmail.com>
2018-04-17 18:28:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function viewCart() {
   var inYour = 'In your cart, you have';
   var viewArray = [];
   var itemsAndPrices = ` ${cart[i][`itemName`]} at $${cart[i][`itemPrice`]}`;
-  var lastItemAndPrice = 
+  var lastItemAndPrice = ` and ${cart[i][`itemName`]} at $${cart[i][`itemPrice`]}`;
   if (cart.length = 0) {
     return "Your shopping cart is empty."
   } else if (cart.length = 1)
