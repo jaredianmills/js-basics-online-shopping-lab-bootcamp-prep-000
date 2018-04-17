ba8830b9814cc17e217dc512a4c57e8b24fcf814
jaredianmills <jaredianmills@gmail.com>
2018-04-17 18:30:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,8 +22,10 @@ function viewCart() {
   var lastItemAndPrice = ` and ${cart[i][`itemName`]} at $${cart[i][`itemPrice`]}`;
   if (cart.length = 0) {
     return "Your shopping cart is empty."
-  } else if (cart.length = 1)
-    for (let i = 0; i < cart.length; i++) {
-            viewArray.push(itemsAndPrices)
+  } else if (cart.length = 1) {
+      for (let i = 0; i < cart.length; i++) {
+        viewArray.push(itemsAndPrices)
+  }
+    return `${inYour}${itemsAndPrices}`
   }
 }
