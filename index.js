945a3ef56de4df575cfb91580427ae641e87c3d8
jaredianmills <jaredianmills@gmail.com>
2018-04-17 20:18:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++) {
     if (item === cart[i]['itemName']) {
       cart.splice(i, 1);
+      itemInCart = true
       return cart;
     }
   }
